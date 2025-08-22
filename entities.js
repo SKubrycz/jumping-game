be6077616053585214a13a9b352b136771573adb
Sebastian Kubrycz <sebastian.kubrycz@gmail.com>
2025-08-22 12:45:10 +0200

    Moved game to a Singleton

--- entities.js
+++ entities.js
@@ -1,3 +1,186 @@
+export class Game {
+  c = document.getElementById("canvas");
+  restart = document.getElementById("restart");
+  restartButton = document.getElementById("restart-button");
+  ctx = null; //c.getContext("2d");
+  w = 0; // c.clientWidth;
+  h = 0; // c.clientHeight;
+  DEFAULT_GAME_HEIGHT = 0; // h - 50;
+  DEFAULT_GAME_VELOCITY_X = 7;
+  BACKGROUND_VELOCITY_X = 3;
+  GRAVITY = 1;
+  obstacleTimeout = null;
+  isGenerated = false;
+  isGameOver = false;
+  gameScore = 0;
+  SCORE_INTERVAL = 250; // ms
+  scoreIntervalTimer = performance.now();
+
+  player = null; // new Player(50, this.DEFAULT_GAME_HEIGHT);
+  obstacles = [];
+  grounds = [];
+  backgrounds = []; // moving background tiles
+
+  constructor(c) {
+    this.c = c;
+    this.ctx = this.c.getContext("2d");
+    this.w = this.c.clientWidth;
+    this.h = this.c.clientHeight;
+    this.DEFAULT_GAME_HEIGHT = this.h - 50;
+    this.player = new Player(50, this.DEFAULT_GAME_HEIGHT);
+    this.restartButton.addEventListener("click", () => this.startGame());
+  }
+
+  jump(e) {
+    if (!this.isGameOver) {
+      if (e.code === "Space" && !this.player.isJumping) {
+        this.player.velocityY = -20;
+        this.player.isJumping = true;
+      }
+    } else {
+      if (e.code === "Space") {
+        this.startGame();
+      }
+    }
+  }
+  updatePlayerPosition() {
+    if (!this.isGameOver && this.player.isJumping) {
+      this.player.velocityY += this.GRAVITY;
+      this.player.coordinates.y += this.player.velocityY;
+
+      if (this.player.coordinates.y >= this.DEFAULT_GAME_HEIGHT) {
+        this.player.coordinates.y =
+          this.DEFAULT_GAME_HEIGHT - this.player.height;
+        this.player.isJumping = false;
+        this.player.velocityY = 0;
+      }
+    }
+  }
+
+  updateObstaclePosition() {
+    if (!this.isGameOver && this.obstacles.length > 0) {
+      for (let i = 0; i < this.obstacles.length; i++) {
+        this.obstacles[i].coordinates.x -= this.DEFAULT_GAME_VELOCITY_X;
+      }
+    }
+  }
+
+  // Sliding ground texture
+  updateGroundPosition() {
+    if (!this.isGameOver && this.grounds.length > 0) {
+      for (let i = 0; i < this.grounds.length; i++) {
+        this.grounds[i].coordinates.x -= this.DEFAULT_GAME_VELOCITY_X;
+
+        if (this.grounds[i].coordinates.x <= -this.grounds[i].width) {
+          this.grounds[i].coordinates.x += 2 * this.w;
+        }
+      }
+    }
+  }
+
+  updateBackgroundPosition() {
+    const bgCount = this.backgrounds.length;
+
+    if (!this.isGameOver && bgCount > 0) {
+      for (let i = 0; i < bgCount; i++) {
+        this.backgrounds[i].coordinates.x -= this.BACKGROUND_VELOCITY_X;
+
+        if (this.backgrounds[i].coordinates.x <= -this.backgrounds[i].dWidth) {
+          this.backgrounds[i].coordinates.x += this.w + this.w / 4;
+        }
+      }
+    }
+  }
+
+  updateGameScore() {
+    if (!this.isGameOver) {
+      const now = performance.now();
+      const diff = now - this.scoreIntervalTimer;
+
+      if (diff >= this.SCORE_INTERVAL) {
+        this.gameScore += 1;
+        this.scoreIntervalTimer = performance.now();
+      }
+    }
+  }
+
+  checkCollision() {
+    this.obstacles.forEach((obstacle) => {
+      if (
+        this.player.coordinates.x < obstacle.coordinates.x + obstacle.width &&
+        this.player.coordinates.x + this.player.width >
+          obstacle.coordinates.x &&
+        this.player.coordinates.y < obstacle.coordinates.y + obstacle.height &&
+        this.player.coordinates.y + this.player.height > obstacle.coordinates.y
+      ) {
+        this.stopGame();
+      }
+    });
+  }
+
+  generateObstacle() {
+    if (!this.isGameOver && !this.isGenerated) {
+      const notVisibleObstacle = this.obstacles.findIndex(
+        (o) => o.coordinates.x < 0
+      );
+      if (notVisibleObstacle != -1) {
+        this.isGenerated = true;
+        const randomTime = Math.floor(Math.random() * 1000 * 2) + 500;
+        this.obstacleTimeout = setTimeout(() => {
+          this.obstacles[notVisibleObstacle].coordinates.x = this.w;
+          this.isGenerated = false;
+        }, randomTime);
+      }
+    }
+  }
+
+  prepareObstacles() {
+    for (let i = 0; i < 2; i++) {
+      this.obstacles.push(new Obstacle(this.w, this.DEFAULT_GAME_HEIGHT));
+    }
+  }
+
+  prepareGround() {
+    for (let i = 0; i < 2; i++) {
+      this.grounds.push(
+        new Ground(i * this.w, this.DEFAULT_GAME_HEIGHT, this.w, this.h)
+      );
+    }
+  }
+
+  prepareBackground() {
+    for (let i = 0; i <= 4; i++) {
+      this.backgrounds.push(
+        new Background(
+          (i * (this.w / 4)) % this.w,
+          0,
+          this.w / 4,
+          100,
+          i * (this.w / 4),
+          this.DEFAULT_GAME_HEIGHT - 100,
+          this.w / 4,
+          100
+        )
+      );
+    }
+  }
+
+  stopGame() {
+    this.isGameOver = true;
+    this.c.style.filter = `blur(2px) grayscale(90%)`;
+    this.restart.style.display = `block`;
+  }
+
+  startGame() {
+    this.isGameOver = false;
+    this.c.style.filter = ``;
+    this.restart.style.display = `none`;
+    this.gameScore = 0;
+    this.obstacles = [];
+    this.prepareObstacles();
+  }
+}
+
 export class Player {
   width = 20;
   height = 20;
--- script.js
+++ script.js
@@ -1,182 +1,20 @@
-import { Player, Obstacle, Ground, Background } from "./entities.js";
+import { Game } from "./entities.js";
 
-const c = document.getElementById("canvas");
-const restartButton = document.getElementById("restart-button");
-const ctx = c.getContext("2d");
-const w = c.clientWidth;
-const h = c.clientHeight;
-const DEFAULT_GAME_HEIGHT = h - 50;
-const DEFAULT_GAME_VELOCITY_X = 7;
-const BACKGROUND_VELOCITY_X = 3;
-const GRAVITY = 1;
-let obstacleTimeout = null;
-let isGenerated = false;
-let isGameOver = false;
-let gameScore = 0;
-const SCORE_INTERVAL = 250; // ms
-let scoreIntervalTimer = performance.now();
-
-const player = new Player(50, DEFAULT_GAME_HEIGHT);
-let obstacles = [];
-let grounds = [];
-let backgrounds = []; // moving background tiles
-
-function jump(e) {
-  if (!isGameOver) {
-    if (e.code === "Space" && !player.isJumping) {
-      player.velocityY = -20;
-      player.isJumping = true;
-    }
-  } else {
-    if (e.code === "Space") {
-      startGame();
-    }
-  }
-}
-function updatePlayerPosition() {
-  if (!isGameOver && player.isJumping) {
-    player.velocityY += GRAVITY;
-    player.coordinates.y += player.velocityY;
-
-    if (player.coordinates.y >= DEFAULT_GAME_HEIGHT) {
-      player.coordinates.y = DEFAULT_GAME_HEIGHT - player.height;
-      player.isJumping = false;
-      player.velocityY = 0;
-    }
-  }
-}
-
-function updateObstaclePosition() {
-  if (!isGameOver && obstacles.length > 0) {
-    for (let i = 0; i < obstacles.length; i++) {
-      obstacles[i].coordinates.x -= DEFAULT_GAME_VELOCITY_X;
-    }
-  }
-}
-
-// Sliding ground texture
-function updateGroundPosition() {
-  if (!isGameOver && grounds.length > 0) {
-    for (let i = 0; i < grounds.length; i++) {
-      grounds[i].coordinates.x -= DEFAULT_GAME_VELOCITY_X;
-
-      if (grounds[i].coordinates.x <= -grounds[i].width) {
-        grounds[i].coordinates.x += 2 * w;
-      }
-    }
-  }
-}
-
-function updateBackgroundPosition() {
-  const bgCount = backgrounds.length;
-
-  if (!isGameOver && bgCount > 0) {
-    for (let i = 0; i < bgCount; i++) {
-      backgrounds[i].coordinates.x -= BACKGROUND_VELOCITY_X;
-
-      if (backgrounds[i].coordinates.x <= -backgrounds[i].dWidth) {
-        backgrounds[i].coordinates.x += w + w / 4;
-      }
-    }
-  }
-}
-
-function updateGameScore() {
-  if (!isGameOver) {
-    const now = performance.now();
-    const diff = now - scoreIntervalTimer;
-
-    if (diff >= SCORE_INTERVAL) {
-      gameScore += 1;
-      scoreIntervalTimer = performance.now();
-    }
-  }
-}
-
-function checkCollision() {
-  obstacles.forEach((obstacle) => {
-    if (
-      player.coordinates.x < obstacle.coordinates.x + obstacle.width &&
-      player.coordinates.x + player.width > obstacle.coordinates.x &&
-      player.coordinates.y < obstacle.coordinates.y + obstacle.height &&
-      player.coordinates.y + player.height > obstacle.coordinates.y
-    ) {
-      stopGame();
-    }
-  });
-}
-
-function generateObstacle() {
-  if (!isGameOver && !isGenerated) {
-    const notVisibleObstacle = obstacles.findIndex((o) => o.coordinates.x < 0);
-    if (notVisibleObstacle != -1) {
-      isGenerated = true;
-      const randomTime = Math.floor(Math.random() * 1000 * 2) + 500;
-      obstacleTimeout = setTimeout(() => {
-        obstacles[notVisibleObstacle].coordinates.x = w;
-        isGenerated = false;
-      }, randomTime);
-    }
-  }
-}
-
-function prepareObstacles() {
-  for (let i = 0; i < 2; i++) {
-    obstacles.push(new Obstacle(w, DEFAULT_GAME_HEIGHT));
-  }
-}
-
-function prepareGround() {
-  for (let i = 0; i < 2; i++) {
-    grounds.push(new Ground(i * w, DEFAULT_GAME_HEIGHT, w, h));
-  }
-}
-
-function prepareBackground() {
-  for (let i = 0; i <= 4; i++) {
-    backgrounds.push(
-      new Background(
-        (i * (w / 4)) % w,
-        0,
-        w / 4,
-        100,
-        i * (w / 4),
-        DEFAULT_GAME_HEIGHT - 100,
-        w / 4,
-        100
-      )
-    );
-  }
-}
-
-function stopGame() {
-  isGameOver = true;
-  c.style.filter = `blur(2px) grayscale(90%)`;
-  restart.style.display = `block`;
-}
-
-function startGame() {
-  isGameOver = false;
-  c.style.filter = ``;
-  restart.style.display = `none`;
-  gameScore = 0;
-  obstacles = [];
-  prepareObstacles();
-}
+const game = new Game(document.getElementById("canvas"));
 
 function draw() {
-  updatePlayerPosition();
-  generateObstacle();
-  updateObstaclePosition();
-  updateGroundPosition();
-  updateBackgroundPosition();
-  checkCollision();
-  updateGameScore();
+  game.updatePlayerPosition();
+  game.generateObstacle();
+  game.updateObstaclePosition();
+  game.updateGroundPosition();
+  game.updateBackgroundPosition();
+  game.checkCollision();
+  game.updateGameScore();
 
-  ctx.fillStyle = "lightblue";
-  ctx.fillRect(0, 0, c.clientWidth, c.clientHeight);
-  backgrounds.forEach((bg) => {
-    ctx.drawImage(
+  game.ctx.fillStyle = "lightblue";
+  game.ctx.fillRect(0, 0, game.c.clientWidth, game.c.clientHeight);
+  game.backgrounds.forEach((bg) => {
+    game.ctx.drawImage(
       bg.texture,
       bg.sliceCoordinates.x,
       bg.sliceCoordinates.y,
@@ -188,34 +26,41 @@ function draw() {
       bg.dHeight
     );
   });
-  ctx.fillStyle = "green";
-  ctx.drawImage(player.texture, player.coordinates.x, player.coordinates.y);
-  ctx.fillStyle = "purple";
-  obstacles.forEach((obstacle) => {
-    ctx.drawImage(
+  game.ctx.fillStyle = "green";
+  game.ctx.drawImage(
+    game.player.texture,
+    game.player.coordinates.x,
+    game.player.coordinates.y
+  );
+  game.ctx.fillStyle = "purple";
+  game.obstacles.forEach((obstacle) => {
+    game.ctx.drawImage(
       obstacle.texture,
       obstacle.coordinates.x,
       obstacle.coordinates.y
     );
   });
-  grounds.forEach((ground) => {
-    ctx.drawImage(ground.texture, ground.coordinates.x, ground.coordinates.y);
+  game.grounds.forEach((ground) => {
+    game.ctx.drawImage(
+      ground.texture,
+      ground.coordinates.x,
+      ground.coordinates.y
+    );
   });
 
-  ctx.font = "20px sans-serif";
-  ctx.fillStyle = "black";
-  ctx.fillText(gameScore, w - 50, 50);
+  game.ctx.font = "20px sans-serif";
+  game.ctx.fillStyle = "black";
+  game.ctx.fillText(game.gameScore, game.w - 50, 50);
   window.requestAnimationFrame(draw);
 }
 
 function main() {
-  scoreIntervalTimer = performance.now();
-  prepareGround();
-  prepareObstacles();
-  prepareBackground();
+  game.scoreIntervalTimer = performance.now();
+  game.prepareGround();
+  game.prepareObstacles();
+  game.prepareBackground();
   draw();
 }
 
 document.addEventListener("DOMContentLoaded", main);
-document.addEventListener("keydown", jump);
-restartButton.addEventListener("click", startGame);
+document.addEventListener("keydown", (e) => game.jump(e));
